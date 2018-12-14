86f1b92837b2d55731dc85a2f8b65950a82bdfc0
tangxing1220 <tangxing1220@outlook.com>
2018-12-14 11:15:42 +0200

    Add new project CSharp_01

---
+++ CSharp_01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from CSharp_01");
+        }
+    }
+}
