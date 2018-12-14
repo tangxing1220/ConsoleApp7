1eec38ea0fe2356dbbeb277bbd67e0f2e0d24617
tangxing_glb <tangxing_glb@outlook.com>
2018-12-14 10:31:31 +0200

    add one new project CSharpStudy_02

---
+++ CSharpStudy_02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello CSharp and GitHub!");
+        }
+    }
+}
